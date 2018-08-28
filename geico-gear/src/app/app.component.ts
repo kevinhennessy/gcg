b6490e5d4e90c7ca1e6c5b79f86e32b690e26001
Kevin.Hennessy <marykevinh@gmail.com>
2018-08-28 14:06:33 -0400

    getting up and running

---
+++ geico-gear/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gcg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'gcg';
+}
