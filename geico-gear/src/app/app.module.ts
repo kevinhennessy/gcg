216346bb671db4b4d73e94fe9736ce548beb672d
Kevin.Hennessy <marykevinh@gmail.com>
2018-09-13 11:04:09 -0400

    Remove LayoutModule

--- geico-gear/src/app/app.module.ts
+++ geico-gear/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatIconModule, MatTabsModule, MatGridListModule } from '@angular/material';
 import { HomeComponent } from './home/home.component';
 import { ProductTileComponent } from './home/product-tile/product-tile.component';
@@ -29,7 +28,6 @@ import { CheckoutComponent } from './checkout/checkout.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    LayoutModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
